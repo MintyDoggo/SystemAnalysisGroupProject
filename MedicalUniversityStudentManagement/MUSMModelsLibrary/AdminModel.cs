8ab912f648cdcbecdf4f3e474ad9796987588584
Brian2524 <b2hinkle@gmail.com>
2021-10-09 16:32:10 -0400

    Make model classes public and add using in imports.razor

--- MedicalUniversityStudentManagement/MUSMModelsLibrary/AdminModel.cs
+++ MedicalUniversityStudentManagement/MUSMModelsLibrary/AdminModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MUSMModelsLibrary
 {
-    class AdminModel
+    public class AdminModel
     {
 
     }
--- MedicalUniversityStudentManagement/MUSMModelsLibrary/StaffModel.cs
+++ MedicalUniversityStudentManagement/MUSMModelsLibrary/StaffModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MUSMModelsLibrary
 {
-    class StaffModel
+    public class StaffModel
     {
 
     }

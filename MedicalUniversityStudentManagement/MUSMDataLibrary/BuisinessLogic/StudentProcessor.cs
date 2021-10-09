a7f26220838c433e7ca8e3c08d4155ad88291b33
Brian2524 <b2hinkle@gmail.com>
2021-10-09 17:40:20 -0400

    scaffold out some function names for StudentProcessor

--- MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/StudentProcessor.cs
+++ MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/StudentProcessor.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MUSMModelsLibrary;
 
 namespace MUSMDataLibrary.BuisinessLogic
 {
     public class StudentProcessor
     {
+        public static async Task<int> CreateStudentAndReturnIdAsync(string connString, string hostIp, string hostServicesAPISocketAddress, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
 
+        public static async Task<int> DeleteStudentByIdAsync(string connString, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<IEnumerable<StudentModel>> GetStudentsAsync(string connString)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

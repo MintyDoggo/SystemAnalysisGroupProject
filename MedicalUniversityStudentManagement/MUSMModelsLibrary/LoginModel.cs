9beb07d043a181577d41151d0382c7a687a3b9e9
Brian2524 <b2hinkle@gmail.com>
2021-10-16 15:13:22 -0400

    Create new model class "LoginModel" from our Component level design doc

---
+++ MedicalUniversityStudentManagement/MUSMModelsLibrary/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace MUSMModelsLibrary
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}

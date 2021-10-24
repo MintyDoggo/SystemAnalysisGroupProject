e8c3caeb6609df3d91e56653d5811375a512e45e
include-iostream <c4hinkle@gmail.com>
2021-10-23 21:05:29 -0300

    for MUSMDataLibrary, implement CreateStudentAndReturnIdAsync() for the StudentProcessor

--- MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/StudentProcessor.cs
+++ MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/StudentProcessor.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using MUSMDataLibrary.DataAccess;
 using MUSMModelsLibrary;
 
 namespace MUSMDataLibrary.BuisinessLogic
@@ -11,7 +14,28 @@ namespace MUSMDataLibrary.BuisinessLogic
     {
         public static async Task<int> CreateStudentAndReturnIdAsync(string connString, string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            // Name of stored procedure to execute
+            string procedureName = "spStudent_CreateAndOutputId";
+
+
+            // Create the Data Table representation of the user defined Student table
+            DataTable studentTable = new DataTable("@inStudent");
+            studentTable.Columns.Add("FirstName", typeof(string));
+            studentTable.Columns.Add("LastName", typeof(string));
+            // Fill in the data
+            studentTable.Rows.Add(firstName, lastName);
+
+            // Make parameters to pass to the stored procedure
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@inStudent", studentTable.AsTableValuedParameter("udtStudent"), DbType.Object);
+            parameters.Add("@outId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+
+            // Execute our stored procedure with the parameters we made
+            await SqlDataAccess.ModifyDataAsync(connString, procedureName, parameters);
+
+            // Return the outputed id from the stored procedure
+            return parameters.Get<int>("@outId");
         }
 
         public static async Task<int> DeleteStudentByIdAsync(string connString, int id)

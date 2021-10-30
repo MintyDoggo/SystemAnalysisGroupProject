096a0e55b8f85f96795b017ec7aa7ce82a433158
Brian2524 <b2hinkle@gmail.com>
2021-10-30 18:39:27 -0400

    implement a working GetStudents endpoint

--- MedicalUniversityStudentManagement/MUSMDatabaseServicesAPI/StudentController.cs
+++ MedicalUniversityStudentManagement/MUSMDatabaseServicesAPI/StudentController.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using MUSMDataLibrary.BuisinessLogic;
+using MUSMModelsLibrary;
 
 namespace MUSMDatabaseServicesAPI
 {
@@ -39,16 +42,14 @@ namespace MUSMDatabaseServicesAPI
         }
 
         [Function("GetStudents")]
-        public static HttpResponseData GetStudents([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+        public static async Task<HttpResponseData> GetStudents([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
 FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("StudentController");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+            IEnumerable<StudentModel> Students = await StudentProcessor.GetStudentsAsync("CONECTION STRING HERE");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteAsJsonAsync<IEnumerable<StudentModel>>(Students);
 
-            response.WriteString("Welcome to Azure Functions!");
 
             return response;
         }

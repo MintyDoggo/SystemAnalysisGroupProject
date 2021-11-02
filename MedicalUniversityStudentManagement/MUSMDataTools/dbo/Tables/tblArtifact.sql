b209bc9b7e714898bbda5bebdaecf09e6e92f8ed
include-iostream <c4hinkle@gmail.com>
2021-11-02 15:44:09 -0300

    make tblArtifact and tblRequiredArtifact for MUSMDataTools

---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/Tables/tblArtifact.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblArtifact]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[RequiredArtifactId] INT NOT NULL FOREIGN KEY REFERENCES tblRequiredArtifact(Id), -- the type of artifact
+	[StudentId] INT NOT NULL FOREIGN KEY REFERENCES tblStudent(Id), -- the owning student
+
+	[DocumentReference] VARCHAR(64) NOT NULL,
+	[CheckedOff] BIT NOT NULL
+)
---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/Tables/tblRequiredArtifact.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblRequiredArtifact]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(32) NOT NULL
+)

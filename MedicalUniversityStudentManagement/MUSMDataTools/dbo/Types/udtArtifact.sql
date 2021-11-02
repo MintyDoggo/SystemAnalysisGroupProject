92c3bcd9ebe2c2f50537ec98fe74fb335b0f46e3
include-iostream <c4hinkle@gmail.com>
2021-11-02 15:57:57 -0300

    make spArtifact_CreateAndOutputId stored procedure and udtArtifact type

---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/Types/udtArtifact.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[udtArtifact] AS TABLE
+(
+	[RequiredArtifactId] INT NOT NULL,
+	[StudentId] INT NOT NULL,
+	[DocumentReference] VARCHAR(64) NOT NULL,
+	[CheckedOff] BIT NOT NULL
+)

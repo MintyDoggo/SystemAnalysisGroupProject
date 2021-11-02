33527611c7efec5d16f110d1e1fdf7d843711493
include-iostream <c4hinkle@gmail.com>
2021-11-02 16:21:47 -0300

    make spArtifact_DeleteById and make CreateArtifactAndReturnIdAsync(), DeleteArtifactByIdAsync(), and GetArtifactsByStudentIdAsync() in the ArtifactProcessor

--- MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/ArtifactProcessor.cs
+++ MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/ArtifactProcessor.cs
@@ -12,6 +12,56 @@ namespace MUSMDataLibrary.BuisinessLogic
 {
     public static class ArtifactProcessor
     {
+        public static async Task<int> CreateArtifactAndReturnIdAsync(string connectionString, ArtifactModel artifact)
+        {
+            // Name of our stored procedure to execute
+            string procedureName = "spArtifact_CreateAndOutputId";
+
+
+            // Create the Data Table representation of the user defined Artifact table
+            DataTable artifactTable = new DataTable("@inArtifact");
+            artifactTable.Columns.Add("RequiredArtifactId", typeof(int));
+            artifactTable.Columns.Add("StudentId", typeof(int));
+            artifactTable.Columns.Add("DocumentReference", typeof(string));
+            artifactTable.Columns.Add("CheckedOff", typeof(bool));
+            // Fill in the data
+            artifactTable.Rows.Add(artifact.RequiredArtifactId, artifact.StudentId, artifact.DocumentReference, artifact.CheckedOff);
+
+            // Make parameters to pass to the stored procedure
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@inArtifact", artifactTable.AsTableValuedParameter("udtArtifact"), DbType.Object);
+            parameters.Add("@outId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+
+            // Execute our stored procedure with the parameters we made
+            await SqlDataAccess.ModifyDataAsync(connectionString, procedureName, parameters);
+
+            // Return the outputed id from the stored procedure
+            return parameters.Get<int>("@outId");
+        }
+
+        public static async Task<int> DeleteArtifactByIdAsync(string connectionString, int id)
+        {
+            string procedureName = "spArtifact_DeleteById";
+
+            // Make parameters to pass to the stored procedure
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@inId", id, dbType: DbType.Int32);
+
+            return await SqlDataAccess.ModifyDataAsync(connectionString, procedureName, parameters);
+        }
+
+        public static async Task<IEnumerable<ArtifactModel>> GetArtifactsByStudentIdAsync(string connectionString, int studentId)
+        {
+            string procedureName = "spArtifact_SelectByStudentId";
+
+            // Make parameters to pass to the stored procedure
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@inStudentId", studentId, dbType: DbType.Int32);
+
+            return await SqlDataAccess.LoadDataAsync<ArtifactModel>(connectionString, procedureName, parameters);
+        }
+
 
     }
 }

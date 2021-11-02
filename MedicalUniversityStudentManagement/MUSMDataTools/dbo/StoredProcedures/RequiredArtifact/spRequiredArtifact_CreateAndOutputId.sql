397da7254a3df23664fcd630c935635698aa2950
include-iostream <c4hinkle@gmail.com>
2021-11-02 16:47:22 -0300

    make spRequiredArtifact_CreateAndOutputId and spRequiredArtifact_DeleteById stored procedures and the udtRequiredArtifact type

---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/StoredProcedures/RequiredArtifact/spRequiredArtifact_CreateAndOutputId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spRequiredArtifact_CreateAndOutputId]
+	@inRequiredArtifact udtRequiredArtifact READONLY,
+	@outId INT OUT
+AS
+BEGIN
+	SET NOCOUNT ON;		-- don't give how many rows affected
+
+	INSERT INTO tblRequiredArtifact([Name]) SELECT [Name] FROM @inRequiredArtifact;
+	SELECT @outId = SCOPE_IDENTITY();
+
+END
+RETURN 0

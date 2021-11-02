6e3190889e333a07d3758bbe7caa84b4cf3574f8
Carter Hinkle <c3hinkle@gmail.com>
2021-11-02 18:54:24 -0400

    changed student table from not null to nullable

--- MedicalUniversityStudentManagement/MUSMDataTools/dbo/Tables/tblStudent.sql
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/Tables/tblStudent.sql
@@ -3,14 +3,14 @@
 	-- Maybe make some of these NULL as part of the extra information the Students need to fill out
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[StaffId] INT NOT NULL FOREIGN KEY REFERENCES tblStaff(Id), -- the associated staff for this student
-	[StudentIdNumber] INT NOT NULL,
-	[FirstName] VARCHAR(64) NOT NULL,
-	[LastName] VARCHAR(64) NOT NULL,
-	[Birthday] DATETIME2 NOT NULL,
-	[Address] VARCHAR(128) NOT NULL,
-	[Major] VARCHAR(32) NOT NULL,
-	[FirstYearEnrolled] INT NOT NULL,
-	[HighSchoolAttended] VARCHAR(64) NOT NULL,
-	[UndergraduateSchoolAttended] VARCHAR(64) NOT NULL
+	[StudentIdNumber] INT,
+	[FirstName] VARCHAR(64),
+	[LastName] VARCHAR(64),
+	[Birthday] DATETIME2,
+	[Address] VARCHAR(128),
+	[Major] VARCHAR(32),
+	[FirstYearEnrolled] INT,
+	[HighSchoolAttended] VARCHAR(64),
+	[UndergraduateSchoolAttended] VARCHAR(64)
 
 )

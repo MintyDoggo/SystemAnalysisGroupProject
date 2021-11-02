23c68cc0ecd94783503228a346d95d0da7c491d2
include-iostream <c4hinkle@gmail.com>
2021-11-02 15:50:25 -0300

    make ArtifactModel and RequiredArtifactModel and ArtifactProcessor (no functionality yet - need to make the stored procedures)

---
+++ MedicalUniversityStudentManagement/MUSMDataLibrary/BuisinessLogic/ArtifactProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MUSMDataLibrary.DataAccess;
+using MUSMModelsLibrary;
+
+namespace MUSMDataLibrary.BuisinessLogic
+{
+    public static class ArtifactProcessor
+    {
+
+    }
+}
---
+++ MedicalUniversityStudentManagement/MUSMModelsLibrary/RequiredArtifactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace MUSMModelsLibrary
+{
+    public class RequiredArtifactModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}

4bfcad279974082a5f982b8079c4638a4b1fa3ed
include-iostream <c4hinkle@gmail.com>
2021-11-02 16:10:58 -0300

    make spArtifact_SelectByStudentId and fix StudentController function name error

---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/StoredProcedures/Artifact/spArtifact_SelectByStudentId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spArtifact_SelectByStudentId]
+	@inStudentId INT
+AS
+BEGIN
+	SET NOCOUNT ON;		-- don't give how many rows affected
+
+	SELECT * FROM [tblArtifact] WHERE StudentId = @inStudentId;
+END
+RETURN 0

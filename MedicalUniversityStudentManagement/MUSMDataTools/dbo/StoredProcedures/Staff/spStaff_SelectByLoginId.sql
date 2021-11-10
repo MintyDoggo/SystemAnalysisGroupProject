f8612e597cf05cd6f06d11411a8dde081d589733
include-iostream <c4hinkle@gmail.com>
2021-11-10 06:55:14 -0400

    make spStudent_SelectByLoginId and spStaff_SelectByLoginId

---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/StoredProcedures/Staff/spStaff_SelectByLoginId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spStaff_SelectByLoginId]
+	@inLoginId INT
+AS
+BEGIN
+	SET NOCOUNT ON;		-- don't give how many rows affected
+
+	SELECT * FROM [tblStaff] WHERE Id = @inLoginId;
+END
+RETURN 0
---
+++ MedicalUniversityStudentManagement/MUSMDataTools/dbo/StoredProcedures/Student/spStudent_SelectByLoginId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spStudent_SelectByLoginId]
+	@inLoginId INT
+AS
+BEGIN
+	SET NOCOUNT ON;		-- don't give how many rows affected
+
+	SELECT * FROM [tblStudent] WHERE Id = @inLoginId;
+END
+RETURN 0

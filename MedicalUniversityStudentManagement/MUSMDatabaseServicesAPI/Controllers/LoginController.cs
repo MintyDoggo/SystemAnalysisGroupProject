ddcacab2d49dbbd6936c1fbd73a71a013d320783
include-iostream <c4hinkle@gmail.com>
2021-11-10 06:48:39 -0400

    change CreateLoginAndReturnId() to handle Staff and Student Logins and scrap the separate CreateStudentLoginAndReturnId() and CreateStaffLoginAndReturnId()

--- MedicalUniversityStudentManagement/MUSMDatabaseServicesAPI/Controllers/LoginController.cs
+++ MedicalUniversityStudentManagement/MUSMDatabaseServicesAPI/Controllers/LoginController.cs
@@ -33,6 +33,7 @@ Example request body:
 
             // Get the body of the request
             string requestBody = await req.ReadAsStringAsync();
+            string queryParameters = req.Url.Query;
 
             // Get the LoginModel from the request body
             LoginModel login;
@@ -58,7 +59,28 @@ Example request body:
             try
             {
                 string connectionString = Environment.GetEnvironmentVariable("SQLConnectionString");
-                retVal = await LoginProcessor.CreateLoginAndReturnIdAsync(connectionString, login);
+
+                switch (login.UserType)
+                {
+                    case MUSMModelsLibrary.Enums.UserType.Staff:
+                        retVal = await LoginProcessor.CreateStaffLoginAndReturnIdAsync(connectionString, login);
+                        break;
+                    case MUSMModelsLibrary.Enums.UserType.Student:
+                        if (queryParameters.Contains("?staffId="))
+                        {
+                            // TODO: This query parameter thing sucks
+                            int staffId = int.Parse(queryParameters.Remove(0, "?staffId=".Length));
+                            retVal = await LoginProcessor.CreateStudentLoginAndReturnIdAsync(connectionString, login, staffId);
+                            break;
+                        }
+                        var badRequestResponse1 = req.CreateResponse(HttpStatusCode.BadRequest);
+                        return badRequestResponse1;
+
+                    default:
+                        var badRequestResponse2 = req.CreateResponse(HttpStatusCode.BadRequest);
+                        return badRequestResponse2;
+                }
+
             }
             catch (Exception e)
             {
@@ -234,156 +256,5 @@ Example query parameters:
             return response;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /**
-         * 
-         * 
-Example request body:
-
-{
-    "Username": "PaulMorton27",
-    "Password": "ihaveasecurepassword",
-    "UserType": 2
-}
-
-         * 
-         */
-        [Function("CreateStudentLoginAndReturnId")]
-        public static async Task<HttpResponseData> CreateStudentLoginAndReturnId([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req, FunctionContext executionContext, int associatedStaffId)
-        {
-            ILogger logger = executionContext.GetLogger("LoginController");
-
-            // Get the body of the request
-            string requestBody = await req.ReadAsStringAsync();
-
-            // Get the LoginModel from the request body
-            LoginModel login;
-            try
-            {
-                login = JsonSerializer.Deserialize<LoginModel>(requestBody);
-                if (login is null)
-                {
-                    throw new JsonException();
-                }
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, e.Message);
-
-                var badRequestResponse = req.CreateResponse(HttpStatusCode.BadRequest);
-                await badRequestResponse.WriteStringAsync("Request didn't meet syntax requirements (make sure you include everything and have the correct property types)");
-                return badRequestResponse;
-            }
-
-            // Call on the data processor and return the Id
-            int retVal;
-            try
-            {
-                string connectionString = Environment.GetEnvironmentVariable("SQLConnectionString");
-                retVal = await LoginProcessor.CreateStudentLoginAndReturnIdAsync(connectionString, login, associatedStaffId);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, e.Message);
-
-                var conflictResponse = req.CreateResponse(HttpStatusCode.Conflict);
-                await conflictResponse.WriteStringAsync("Conflict when inserting into the database");
-                return conflictResponse;
-            }
-
-
-            // Successfully added the Login to the database
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            await response.WriteAsJsonAsync(retVal);
-            return response;
-        }
-
-
-
-
-
-        /**
- * 
- * 
-Example request body:
-
-{
-"Username": "PaulMorton27",
-"Password": "ihaveasecurepassword",
-"UserType": 2
-}
-
- * 
- */
-        [Function("CreateStaffLoginAndReturnId")]
-        public static async Task<HttpResponseData> CreateStaffLoginAndReturnId([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req, FunctionContext executionContext)
-        {
-            ILogger logger = executionContext.GetLogger("LoginController");
-
-            // Get the body of the request
-            string requestBody = await req.ReadAsStringAsync();
-
-            // Get the LoginModel from the request body
-            LoginModel login;
-            try
-            {
-                login = JsonSerializer.Deserialize<LoginModel>(requestBody);
-                if (login is null)
-                {
-                    throw new JsonException();
-                }
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, e.Message);
-
-                var badRequestResponse = req.CreateResponse(HttpStatusCode.BadRequest);
-                await badRequestResponse.WriteStringAsync("Request didn't meet syntax requirements (make sure you include everything and have the correct property types)");
-                return badRequestResponse;
-            }
-
-            // Call on the data processor and return the Id
-            int retVal;
-            try
-            {
-                string connectionString = Environment.GetEnvironmentVariable("SQLConnectionString");
-                retVal = await LoginProcessor.CreateStaffLoginAndReturnIdAsync(connectionString, login);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, e.Message);
-
-                var conflictResponse = req.CreateResponse(HttpStatusCode.Conflict);
-                await conflictResponse.WriteStringAsync("Conflict when inserting into the database");
-                return conflictResponse;
-            }
-
-
-            // Successfully added the Login to the database
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            await response.WriteAsJsonAsync(retVal);
-            return response;
-        }
     }
-
-
-
 }
